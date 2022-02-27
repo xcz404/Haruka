9f47484c0916048d9a5d4b9005f88ba2afb68d81
Sayu <86679476+xcz404@users.noreply.github.com>
2022-02-28 06:15:36 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="ZeeoneOfc"
+global.ownername ="Rafvnss"
 global.ownernumber = "62887435047326"
-global.botname = "Haruka-Bot"
+global.botname = "V9-Bot"
 global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'RIFQIBOTZ' //apikey
